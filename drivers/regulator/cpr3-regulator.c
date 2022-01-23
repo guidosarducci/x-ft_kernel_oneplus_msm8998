8a59c03e402b5af7c65f9b265a526fa6ced3644f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-01-23 06:44:01 +0600

    regulator: cpr3-regulator: Don't create debugfs when !CONFIG_DEBUG_FS
    Additionally, compile out unused codes while debug_fs is disabled.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/regulator/cpr3-regulator.c
+++ drivers/regulator/cpr3-regulator.c
@@ -355,7 +355,9 @@
 
 static DEFINE_MUTEX(cpr3_controller_list_mutex);
 static LIST_HEAD(cpr3_controller_list);
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *cpr3_debugfs_base;
+#endif
 
 /* UGLY UGLY UGLY HACK for fast bringup */
 #define CPR3_MAX_INSTANCES		15
@@ -5129,6 +5131,7 @@ static int cpr3_regulator_vreg_register(struct cpr3_regulator *vreg)
 	return 0;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static int debugfs_int_set(void *data, u64 val)
 {
 	*(int *)data = val;
@@ -6197,6 +6200,7 @@ static void cpr3_regulator_debugfs_ctrl_remove(struct cpr3_controller *ctrl)
 		debugfs_remove_recursive(ctrl->debugfs);
 	}
 }
+#endif
 
 /**
  * cpr3_regulator_init_ctrl_data() - performs initialization of CPR controller
@@ -6633,7 +6637,9 @@ int cpr3_regulator_register(struct platform_device *pdev,
 	}
 
 	mutex_lock(&cpr3_controller_list_mutex);
+#ifdef CONFIG_DEBUG_FS
 	cpr3_regulator_debugfs_ctrl_add(ctrl);
+#endif
 	list_add(&ctrl->list, &cpr3_controller_list);
 	mutex_unlock(&cpr3_controller_list_mutex);
 
@@ -6668,7 +6674,9 @@ int cpr3_regulator_unregister(struct cpr3_controller *ctrl)
 
 	mutex_lock(&cpr3_controller_list_mutex);
 	list_del(&ctrl->list);
+#ifdef CONFIG_DEBUG_FS
 	cpr3_regulator_debugfs_ctrl_remove(ctrl);
+#endif
 	mutex_unlock(&cpr3_controller_list_mutex);
 
 	cpuhp_remove_state_nocalls(CPUHP_AP_QCOM_CPR3_ONLINE);
