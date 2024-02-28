1beec8d3be61000ade1de493d3e67ebef64ddf12
Juansheng <jaycopter@foxmail.com>
2024-02-28 13:22:01 +0800

    sched/fair: Removed useless update of p->recent_used_cpu
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7523,11 +7523,6 @@ static inline int __select_idle_sibling(struct task_struct *p, int prev, int tar
 	    cpus_share_cache(recent_used_cpu, target) &&
 	    (available_idle_cpu(recent_used_cpu) || sched_idle_cpu(recent_used_cpu)) &&
 	    cpumask_test_cpu(p->recent_used_cpu, &p->cpus_allowed)) {
-		/*
-		 * Replace recent_used_cpu with prev as it is a potential
-		 * candidate for the next wake:
-		 */
-		p->recent_used_cpu = prev;
 		return recent_used_cpu;
 	}
 
