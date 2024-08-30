e05447c9c6a16567373220da13ea83135197782b
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-08-30 10:08:52 -0400

    clk: qcom: gcc/mmcc-msm8998: Do not try to configure votable PLLs
    These are not supposed to be configured or reconfigured at probe
    and should only be FSM voted. Downstream only defines masks or
    PLL config for non-votable PLLs due to votable PLLs different ops
    which explicitly only limits the clock framework to only perform FSM
    voting.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/clk/qcom/gcc-msm8998.c
+++ drivers/clk/qcom/gcc-msm8998.c
@@ -155,25 +155,6 @@ static struct pll_vco fabia_vco[] = {
 
 static unsigned int soft_vote_gpll0;
 
-/*
-static const struct alpha_pll_config gpll0_config = {
-	.l = 0x1F,
-	.alpha = 0x4000,
-	.config_ctl_val = 0x20485699,
-	.config_ctl_hi_val = 0x00002067,
-	.user_ctl_val = 0x1,
-	.user_ctl_hi_val = 0x00004805,
-};
-*/
-
-static const struct alpha_pll_config gpll4_config = {
-	.l = 0x13,
-	.config_ctl_val = 0x20485699,
-	.config_ctl_hi_val = 0x00002067,
-	.user_ctl_val = 0x1001,
-	.user_ctl_hi_val = 0x00004805,
-};
-
 static struct clk_alpha_pll gpll0 = {
 	.offset = 0x0,
 	.regs = clk_alpha_pll_regs[CLK_ALPHA_PLL_TYPE_FABIA],
@@ -182,7 +163,6 @@ static struct clk_alpha_pll gpll0 = {
 	.soft_vote = &soft_vote_gpll0,
 	.soft_vote_mask = PLL_SOFT_VOTE_PRIMARY,
 	.flags = SUPPORTS_FSM_VOTE,
-	/* .config = &gpll0_config, */
 	.clkr = {
 		.enable_reg = 0x52000,
 		.enable_mask = BIT(0),
@@ -243,7 +223,6 @@ static struct clk_alpha_pll gpll4 = {
 	.regs = clk_alpha_pll_regs[CLK_ALPHA_PLL_TYPE_FABIA],
 	.vco_table = fabia_vco,
 	.num_vco = ARRAY_SIZE(fabia_vco),
-	.config = &gpll4_config,
 	.clkr = {
 		.enable_reg = 0x52000,
 		.enable_mask = BIT(4),
@@ -3269,10 +3248,6 @@ static int gcc_msm8998_probe(struct platform_device *pdev)
 		if (ret)
 			return ret;
 	}
-
-	// Do not configure GPLL0, or hypervisor will fault!!
-	//clk_fabia_pll_configure(&gpll0, regmap, &gpll0_config);
-	clk_fabia_pll_configure(&gpll4, regmap, &gpll4_config);
 
 	/*
 	 * GCC_MMSS_MISC - GCC_GPU_MISC:
