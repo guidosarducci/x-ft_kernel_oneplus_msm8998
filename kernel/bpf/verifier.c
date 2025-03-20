1072784971f71356e5c3fa60e9df32c0aac4f88e
Roberto Sartori <roberto.sartori.android@gmail.com>
2025-03-20 23:28:13 +0100

    [FIXUP] bpf: write back the verifier log buffer as it gets filled
    This was probably a copy-paste mistake during some bpf portings, and it
    caused kernel panics using BTF.
    
    Fix this properly (following mainline commit: 'bpf: write back the verifier log buffer as it gets filled'
    https://github.com/torvalds/linux/commit/a2a7d5701052542cd2260e7659b12443e0a74733
    )
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -7164,9 +7164,6 @@ int bpf_check(struct bpf_prog **prog, union bpf_attr *attr,
 			goto err_unlock;
 
 		ret = -ENOMEM;
-		log->kbuf = vmalloc(log->len_total);
-		if (!log->kbuf)
-			goto err_unlock;
 	}
 
 	env->strict_alignment = !!(attr->prog_flags & BPF_F_STRICT_ALIGNMENT);
