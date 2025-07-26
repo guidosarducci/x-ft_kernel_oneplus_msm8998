1f5f444e0d21a3202f18b232d9f2377b0cb11687
Edrick Sinsuan <evcsinsuan@gmail.com>
2025-07-26 01:53:30 -0400

    gpu: msm: a5xx: Fix a5xx_read_throttling_counters
    We need to have the adj variable set as a signed integer in order
    to detect if we have spent more cycles at 10% FW throttle than >50%
    CRC throttling. This is important in deciding whether the counter
    is to be included or dropped in calculating GPU busy.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/gpu/msm/adreno_a5xx.c
+++ drivers/gpu/msm/adreno_a5xx.c
@@ -1697,7 +1697,7 @@ static int a5xx_enable_pwr_counters(struct adreno_device *adreno_dev,
 static int64_t a5xx_read_throttling_counters(struct adreno_device *adreno_dev)
 {
 	int i;
-	uint64_t adj;
+	int64_t adj;
 	uint64_t th[ADRENO_GPMU_THROTTLE_COUNTERS];
 	struct adreno_busy_data *busy = &adreno_dev->busy_data;
 
