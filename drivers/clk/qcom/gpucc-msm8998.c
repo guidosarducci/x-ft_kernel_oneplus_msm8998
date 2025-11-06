2f6622e0966a984b3c01e60d73f177124a0a1b18
Edrick Sinsuan <evcsinsuan@gmail.com>
2025-11-06 14:38:14 -0500

    clk: qcom: gpucc-msm8998: Complete and cleanup clocks configuration
    Let's complete all the missing clock configurations for GPU and cleanup
    to ensure clear and proper configuration.
    
    Primarily, gpucc_cxo_clk (gpucc_xo) has been renamed and configured
    completely as it was missing its enable_reg and enable_mask which can
    cause issues when the hardware expects a vote on the branch. Also follow
    newer QCOM SoCs configuration with the clk framework wherein we decouple the
    gpucc_cxo_clk from the other clocks as we only need to vote on this
    clock, use bi_tcxo_ao as a parent to the GPU clocks which require clock
    rate scaling.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/clk/qcom/gpucc-msm8998.c
+++ drivers/clk/qcom/gpucc-msm8998.c
@@ -67,7 +67,7 @@ static DEFINE_VDD_REGULATORS(vdd_gpucc, VDD_GFX_MAX, 1, vdd_gpucc_corner);
 static DEFINE_VDD_REGULATORS(vdd_gpucc_mx, VDD_MX_NUM, 1, vdd_corner);
 
 enum {
-	P_GPU_XO,
+	P_BI_TCXO,
 	P_GPLL0,
 	P_GPU_CC_PLL0_OUT_EVEN,
 	P_GPU_CC_PLL0_OUT_MAIN,
@@ -75,36 +75,23 @@ enum {
 };
 
 static const struct parent_map gpucc_parent_map_0[] = {
-	{ P_GPU_XO, 0 },
+	{ P_BI_TCXO, 0 },
 	{ P_CRC_DIV,  1 },
 };
 
 static const char * const gpucc_parent_names_0[] = {
-	"gpucc_xo",
+	"bi_tcxo_ao",
 	"crc_div",
 };
 
 static const struct parent_map gpucc_parent_map_1[] = {
-	{ P_GPU_XO, 0 },
+	{ P_BI_TCXO, 0 },
 	{ P_GPLL0, 5 },
 };
 
 static const char * const gpucc_parent_names_1[] = {
-	"gpucc_xo",
+	"bi_tcxo_ao",
 	"gcc_gpu_gpll0_clk",
-};
-
-static struct clk_branch gpucc_xo = {
-	.halt_reg = 0x01020,
-	.clkr.hw.init = &(struct clk_init_data) {
-		.name = "gpucc_xo",
-		.parent_names = (const char*[]) {
-			"bi_tcxo_ao",
-		},
-		.num_parents = 1,
-		.ops = &clk_branch2_ops,
-		.flags = CLK_IS_CRITICAL,
-	},
 };
 
 static struct pll_vco fabia_vco[] = {
@@ -129,7 +116,7 @@ static struct clk_alpha_pll gpu_pll0_pll = {
 	.config = &gpu_pll0_config,
 	.clkr.hw.init = &(struct clk_init_data) {
 		.name = "gpu_cc_pll0",
-		.parent_names = (const char *[]){ "gpucc_xo" },
+		.parent_names = (const char *[]){ "bi_tcxo_ao" },
 		.num_parents = 1,
 		.ops = &clk_alpha_pll_fabia_ops,
 		VDD_GPU_MX_FMAX_MAP4(
@@ -197,7 +184,7 @@ static struct clk_init_data gfx3d_clk_data = {
 };
 
 static struct clk_rcg2 gfx3d_clk_src = {
-	.cmd_rcgr = 0x01070,
+	.cmd_rcgr = 0x1070,
 	.hid_width = 5,
 	.parent_map = gpucc_parent_map_0,
 	.freq_tbl = ftbl_gfx3d_clk_src,
@@ -206,12 +193,12 @@ static struct clk_rcg2 gfx3d_clk_src = {
 };
 
 static struct freq_tbl ftbl_rbbmtimer_clk_src[] = {
-	F( 19200000,  P_GPU_XO,    1,    0,     0),
+	F( 19200000, P_BI_TCXO,    1,    0,     0),
 	{ }
 };
 
 static struct clk_rcg2 rbbmtimer_clk_src = {
-	.cmd_rcgr = 0x010B0,
+	.cmd_rcgr = 0x10B0,
 	.hid_width = 5,
 	.parent_map = gpucc_parent_map_1,
 	.freq_tbl = ftbl_rbbmtimer_clk_src,
@@ -225,7 +212,7 @@ static struct clk_rcg2 rbbmtimer_clk_src = {
 };
 
 static struct freq_tbl ftbl_gfx3d_isense_clk_src[] = {
-	F(  19200000,  P_GPU_XO,    1,    0,     0),
+	F(  19200000, P_BI_TCXO,    1,    0,     0),
 	F(  40000000,   P_GPLL0,   15,    0,     0),
 	F( 200000000,   P_GPLL0,    3,    0,     0),
 	F( 300000000,   P_GPLL0,    2,    0,     0),
@@ -233,7 +220,7 @@ static struct freq_tbl ftbl_gfx3d_isense_clk_src[] = {
 };
 
 static struct clk_rcg2 gfx3d_isense_clk_src = {
-	.cmd_rcgr = 0x01100,
+	.cmd_rcgr = 0x1100,
 	.hid_width = 5,
 	.parent_map = gpucc_parent_map_1,
 	.freq_tbl = ftbl_gfx3d_isense_clk_src,
@@ -248,13 +235,13 @@ static struct clk_rcg2 gfx3d_isense_clk_src = {
 };
 
 static struct freq_tbl ftbl_rbcpr_clk_src[] = {
-	F( 19200000,  P_GPU_XO,    1,    0,     0),
+	F( 19200000, P_BI_TCXO,    1,    0,     0),
 	F( 50000000,   P_GPLL0,   12,    0,     0),
 	{ }
 };
 
 static struct clk_rcg2 rbcpr_clk_src = {
-	.cmd_rcgr = 0x01030,
+	.cmd_rcgr = 0x1030,
 	.hid_width = 5,
 	.parent_map = gpucc_parent_map_1,
 	.freq_tbl = ftbl_rbcpr_clk_src,
@@ -269,6 +256,7 @@ static struct clk_rcg2 rbcpr_clk_src = {
 
 static struct clk_branch gpucc_gfx3d_clk = {
 	.halt_reg = 0x1098,
+	.halt_check = BRANCH_HALT,
 	.clkr = {
 		.enable_reg = 0x1098,
 		.enable_mask = BIT(0),
@@ -281,11 +269,12 @@ static struct clk_branch gpucc_gfx3d_clk = {
 			.flags = CLK_SET_RATE_PARENT,
 			.ops = &clk_branch2_ops,
 		},
-	}
+	},
 };
 
 static struct clk_branch gpucc_rbbmtimer_clk = {
 	.halt_reg = 0x10D0,
+	.halt_check = BRANCH_HALT,
 	.clkr = {
 		.enable_reg = 0x10D0,
 		.enable_mask = BIT(0),
@@ -303,6 +292,7 @@ static struct clk_branch gpucc_rbbmtimer_clk = {
 
 static struct clk_branch gpucc_gfx3d_isense_clk = {
 	.halt_reg = 0x1124,
+	.halt_check = BRANCH_HALT,
 	.clkr = {
 		.enable_reg = 0x1124,
 		.enable_mask = BIT(0),
@@ -318,10 +308,29 @@ static struct clk_branch gpucc_gfx3d_isense_clk = {
 	},
 };
 
-static struct clk_branch gpucc_rbcpr_clk = {
-	.halt_reg = 0x01054,
+static struct clk_branch gpucc_cxo_clk = {
+	.halt_reg = 0x1020,
+	.halt_check = BRANCH_HALT,
 	.clkr = {
-		.enable_reg = 0x01054,
+		.enable_reg = 0x1020,
+		.enable_mask = BIT(0),
+		.hw.init = &(struct clk_init_data) {
+			.name = "gpucc_cxo_clk",
+			.parent_names = (const char*[]) {
+				"bi_tcxo_ao",
+			},
+			.num_parents = 1,
+			.ops = &clk_branch2_ops,
+			.flags = CLK_IS_CRITICAL,
+		},
+	},
+};
+
+static struct clk_branch gpucc_rbcpr_clk = {
+	.halt_reg = 0x1054,
+	.halt_check = BRANCH_HALT,
+	.clkr = {
+		.enable_reg = 0x1054,
 		.enable_mask = BIT(0),
 		.hw.init = &(struct clk_init_data) {
 			.name = "gpucc_rbcpr_clk",
@@ -342,7 +351,7 @@ static struct clk_regmap *gpucc_msm8998_clocks[] = {
 };
 
 static struct clk_regmap *gpucc_msm8998_early_clocks[] = {
-	[GPUCC_XO] = &gpucc_xo.clkr,
+	[GPUCC_XO] = &gpucc_cxo_clk.clkr,
 	[RBCPR_CLK_SRC] = &rbcpr_clk_src.clkr,
 	[GPUCC_RBCPR_CLK] = &gpucc_rbcpr_clk.clkr,
 	[RBBMTIMER_CLK_SRC] = &rbbmtimer_clk_src.clkr,
